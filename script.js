0117a839cee38740e5e04b6b1b2194ae52d98123
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-20 11:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function updater()
+{
+	const now = new Date();
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+		second:'2-digit',
+		hour12: true;
+	};
+	const formattedDate =  now.toLocaleString('en-US', options);
+document.getElementById('timer').innerText = formattedDate;
+}
+setInterval(updatetime, 1000);
+updateTime();
